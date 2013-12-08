8a384303e7e0c148708e665769eb19cfbad785f8
Kuba Sejdak <kuba.sejdak@gmail.com>
2013-12-08 21:11:17 +0100

    Added Lib to CMake. Added test md5 call to TableGenerator.

--- Lib/md5.h
+++ Lib/md5.h
@@ -1,7 +1,10 @@
 #ifndef _MD5_H
 #define _MD5_H
 
-extern void md5(const uint8_t *initial_msg, size_t initial_len,
-		uint8_t *digest);
+#include <stdint.h>
+
+#define MD5_DIGEST_LEN 16
+
+extern void md5(const uint8_t *initial_msg, size_t initial_len, uint8_t *digest);
 
 #endif
--- TablesGenerator/main.c
+++ TablesGenerator/main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "md5.h"
 
 int main(int argc, char* argv[])
 {
-    printf("Hello world");
+    const char* initial_msg = "wypierdalaj dziadu z PORR...";
+    uint8_t* md5_digest = (uint8_t*) malloc(MD5_DIGEST_LEN);
 
+    md5(initial_msg, strlen(initial_msg), md5_digest);
+
+    printf("MD5(%s) = %s", initial_msg, (char*) md5_digest);
+
+    free(md5_digest);
     return 0;
 }
