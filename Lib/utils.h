9464801209a89cbf8f39d1e0c2cbb8e020608471
Kuba Sejdak <kuba.sejdak@gmail.com>
2013-12-09 22:04:47 +0100

    Added function for storing table rows.

--- Lib/utils.h
+++ Lib/utils.h
@@ -1,19 +1,29 @@
 #ifndef _UTILS_H
 #define _UTILS_H
 
+#include <stdio.h>
+#include <string.h>
+#include "md5.h"
+
+#define TABLE_FILE  "rainbow_table.txt"
 #define MAX_PASSWD	8
 
 typedef unsigned int checksum_t[4];
 typedef char password_t[MAX_PASSWD + 1];
 
-static inline void checksum(checksum_t *out, const password_t *in)
+static void checksum(checksum_t* out, const password_t* in)
+{
+    md5(in, strlen(in), out);
+}
+
+static void reduce(password_t* out, const checksum_t* in)
 {
 
 }
 
-static inline void reduce(password_t *out, const checksum_t *in)
+static void printDigest(checksum_t* checksum, const password_t* password)
 {
-
+    printf("MD5(%s) = %x%x%x%x\n", password, checksum[0], checksum[1], checksum[2], checksum[3]);
 }
 
 #endif
--- TablesGenerator/main.c
+++ TablesGenerator/main.c
@@ -1,18 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "md5.h"
+#include "utils.h"
+
+// Rainbow Table file format:
+// ----------------------------------------------------------------------
+// password   : MD5 hash   : number of iterations from password to hash
+// ----------------------------------------------------------------------
+// password_t : checksum_t : unsigned int
+
+void storeTableRow(const password_t* password, checksum_t* checksum, unsigned int iterations);
 
 int main(int argc, char* argv[])
 {
-    const char* initial_msg = "wypierdalaj dziadu z PORR...";
-    uint8_t* md5_digest = (uint8_t*) malloc(MD5_DIGEST_LEN);
+    checksum_t md5_digest;
+    password_t testPassword = "12345678";
 
-    md5(initial_msg, strlen(initial_msg), md5_digest);
+    checksum(md5_digest, testPassword);
+    printDigest(md5_digest, testPassword);
 
-    printf("MD5(%s) = %s", initial_msg, (char*) md5_digest);
+    storeTableRow(testPassword, md5_digest, 5);
 
-    free(md5_digest);
     return 0;
+}
+
+void storeTableRow(const password_t* password, checksum_t* checksum, unsigned int iterations)
+{
+    FILE* file = fopen(TABLE_FILE, "a+");
+    if(!file)
+    {
+        perror("Error opening file");
+    }
+    
+    fprintf(file, "%s : %x%x%x%x : %d\n", password, checksum[0], checksum[1], checksum[2], checksum[3], iterations);
+    fclose(file);
 }
