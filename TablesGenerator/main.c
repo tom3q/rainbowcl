6ef7fd9ad65ec4243c3a9793c28c45a0dee7b2c2
Kuba Sejdak <kuba.sejdak@gmail.com>
2013-12-14 14:41:23 +0100

    Added checking if password is unique.

--- TablesGenerator/main.c
+++ TablesGenerator/main.c
@@ -9,6 +9,8 @@
 #define NUMBER_OF_PASSWORDS     1000
 #define LENGTH_OF_CHAIN         1000
 
+const char** uniquePasswords[NUMBER_OF_PASSWORDS];
+
 // Rainbow Table file format:
 // ----------------------------------------------------------------------
 // password   : MD5 hash   : number of iterations from password to hash
@@ -16,18 +18,23 @@
 // password_t : checksum_t : unsigned int
 
 // stores one rainbow table row into file
-void storeTableRow(const password_t* password, checksum_t* checksum, unsigned int iterations);
+void storeTableRow(const password_t password, checksum_t checksum, unsigned int iterations);
 
 // generates random string with MAX_PASSWD length
 void randomString(char* out);
 
 // generates one rainbow table chain, results in ont rainbow table row
-void generateRainbowTableChain(const password_t* password);
+void generateRainbowTableChain(const password_t password);
+
+// checks is generated password is unique in rainbow table
+// returns 1 if true, 0 otherwise
+int isPasswordUnique(const password_t password);
 
 int main(void)
 {
     int i;
 
+    // init program
     srand(time(NULL));
     clock_t startTime = clock();
 
@@ -39,6 +46,9 @@ int main(void)
 
         password_t randomPassword;
         randomString(randomPassword);
+        while(!isPasswordUnique(randomPassword))
+            randomString(randomPassword);
+
         generateRainbowTableChain(randomPassword);
     }
     printf("\b\b\b\b\b100 %%\n");
@@ -79,13 +89,13 @@ void randomString(char* out)
     *out = '\0';
 }
 
-void generateRainbowTableChain(const password_t* initialPassword)
+void generateRainbowTableChain(const password_t initialPassword)
 {
     checksum_t md5_digest;
     int i;
 
     password_t chainPassword;
-    strcpy(chainPassword, *initialPassword, MAX_PASSWD);
+    strcpy(chainPassword, initialPassword, MAX_PASSWD);
     chainPassword[MAX_PASSWD] = '\0';
 
     for(i = 0; i < LENGTH_OF_CHAIN; ++i)
@@ -95,4 +105,19 @@ void generateRainbowTableChain(const password_t* initialPassword)
     }
 
     storeTableRow(initialPassword, md5_digest, LENGTH_OF_CHAIN);
+}
+
+int isPasswordUnique(const password_t password)
+{
+    int i;
+    for(i = 0; i < NUMBER_OF_PASSWORDS; ++i)
+    {
+        if(uniquePasswords[i] == NULL)
+            return 1;
+
+        if(!strcmp(uniquePasswords[i], password))
+            return 1;
+    }
+
+    return 0;
 }
