be864b7f4824b1deaa298e6a9790f608012e65ed
Tomasz Figa <tomasz.figa@gmail.com>
2013-12-15 16:02:58 +0100

    Rework table generation to be threaded properly

--- Lib/utils.h
+++ Lib/utils.h
@@ -3,20 +3,21 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <sys/time.h>
 #include "md5.h"
 
-#define TABLE_FILE  "rainbow_table.txt"
-#define MAX_PASSWD	8
+#define TABLE_FILE      "rainbow.tbl"
+#define MAX_PASSWD      5
 
-typedef unsigned int checksum_t[4];
+typedef unsigned int hash_t[4];
 typedef char password_t[MAX_PASSWD + 1];
 
-static void checksum(checksum_t* out, password_t* const in)
+static inline void hash(hash_t out, password_t const in)
 {
-    md5(*in, strlen(*in), *out);
+    md5(in, strlen(in), out);
 }
 
-static void reduce(password_t out, checksum_t const in)
+static inline void reduce(password_t out, hash_t const in)
 {
     // hacky: this is not a correct reduce function, resulting password may not fit in our character set
     out[0] = (in[0] & 0x00FF0000) >> 16;
@@ -30,9 +31,9 @@ static void reduce(password_t out, checksum_t const in)
     out[8] = '\0';
 }
 
-static void printDigest(checksum_t checksum, password_t const password)
+static inline void printDigest(hash_t hash, password_t const password)
 {
-    printf("MD5(%s) = %x%x%x%x\n", password, checksum[0], checksum[1], checksum[2], checksum[3]);
+    printf("MD5(%s) = %x%x%x%x\n", password, hash[0], hash[1], hash[2], hash[3]);
 }
 
 #define USEC_PER_SEC            1000000
--- TablesGenerator/main.c
+++ TablesGenerator/main.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,149 +9,169 @@
 
 // parallel settings
 #define OPENMP_MODE 1
-#define INTEL_CILK_MODE 0
+#define CILK_MODE 0
 
-#if OPENMP_MODE == 1 && INTEL_CILK_MODE == 1
-    #error "Cannot compile in both OpenMP and Intel Cilk+ mode"
+#if OPENMP_MODE && CILK_MODE
+    #error "Cannot compile in both OpenMP and Cilk+ mode"
 #endif
 
+#define NUMBER_OF_PASSWORDS     1000000000
+#define CHAINS_IN_BLOCK         1000
+#define LENGTH_OF_CHAIN         10000
+#define NUMBER_OF_CHAINS        (NUMBER_OF_PASSWORDS / LENGTH_OF_CHAIN)
+#define NUMBER_OF_BLOCKS        (NUMBER_OF_CHAINS / CHAINS_IN_BLOCK)
 
-#define NUMBER_OF_PASSWORDS     1000
-#define LENGTH_OF_CHAIN         1000
-
-const char** uniquePasswords[NUMBER_OF_PASSWORDS];
+#if NUMBER_OF_BLOCKS == 0
+#undef NUMBER_OF_BLOCKS
+#define NUMBER_OF_BLOCKS        1
+#endif
 
 // Rainbow Table file format:
 // ----------------------------------------------------------------------
-// password   : MD5 hash   : number of iterations from password to hash
+// chain length : MD5 hash : password
 // ----------------------------------------------------------------------
-// password_t : checksum_t : unsigned int
+// uint32_t     : hash_t   : password_t
+struct rainbow_chain {
+    uint32_t length;
+    hash_t hash;
+    password_t password;
+};
 
 // stores one rainbow table row into file
-void storeTableRow(const password_t password, checksum_t checksum, unsigned int iterations);
-
-// generates random string with MAX_PASSWD length
-void randomString(char* out);
-
-// generates one rainbow table chain, results in ont rainbow table row
-void generateRainbowTableChain(const password_t password);
-
-// checks is generated password is unique in rainbow table
-// returns 1 if true, 0 otherwise
-int isPasswordUnique(const password_t password);
-
-// saves unique password
-void saveUniquePassword(const password_t password);
-
-int main(void)
+static void storeTableChain(struct rainbow_chain *chain)
 {
-    int i;
-
-    // init program
-    srand(time(NULL));
-    clock_t startTime = clock();
-
-#if OPENMP_MODE == 1
-    printf("OpenMP mode selected.\n");
-    #pragma omp parallel for
-#elif INTEL_CILK_MODE == 1
-    printf("Intel Cilk+ mode selected.\n");
-#else
-    printf("No parallel mode selected.\n");
-#endif
-    for(i = 0; i < NUMBER_OF_PASSWORDS; ++i)
-    {
-        password_t randomPassword;
-        randomString(randomPassword);
-        while(!isPasswordUnique(randomPassword))
-            randomString(randomPassword);
-        saveUniquePassword(randomPassword);
-
-        generateRainbowTableChain(randomPassword);
-    }
-
-    clock_t endTime = clock();
-    float workTimeSeconds = (float) (endTime - startTime) / CLOCKS_PER_SEC;
-    printf("Work time: %.2f sec\n", workTimeSeconds);
-
-    for(i = 0; i < NUMBER_OF_PASSWORDS; ++i)
-        free(uniquePasswords[i]);
-
-    return 0;
-}
-
-void storeTableRow(const password_t password, checksum_t checksum, unsigned int iterations)
-{
-    FILE* file = fopen(TABLE_FILE, "a+");
+    FILE* file = fopen(TABLE_FILE, "ab");
     if(!file)
     {
         perror("Error opening file");
         exit(1);
     }
-    
-    fprintf(file, "%s : %08x%08x%08x%08x : %d\n", password, checksum[0], checksum[1], checksum[2], checksum[3], iterations);
+
+    fwrite(chain, sizeof(*chain), 1, file);
     fclose(file);
 }
 
-void randomString(char* out)
+// generates random string with up to MAX_PASSWD length
+static void randomString(char* out)
 {
-    char charset[] = "0123456789"
-                     "abcdefghijklmnopqrstuvwxyz"
-                     "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    static const char charset[] = "0123456789"
+                                  "abcdefghijklmnopqrstuvwxyz"
+                                  "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    size_t length = MAX_PASSWD;
+    size_t length = (size_t) ((double) rand() / RAND_MAX * MAX_PASSWD);
     while(length-- > 0)
     {
-        size_t index = (size_t) ((double) rand() / RAND_MAX * (sizeof(charset) - 1));
+        size_t index = (size_t) ((double) rand() /
+                                            RAND_MAX * (sizeof(charset) - 1));
         *out++ = charset[index];
     }
 
     *out = '\0';
 }
 
-void generateRainbowTableChain(const password_t initialPassword)
+// generates one rainbow table chain, results in ont rainbow table row
+static void generateRainbowTableChain(struct rainbow_chain *chain)
 {
-    checksum_t md5_digest;
+    password_t chainPassword;
+    hash_t passwordHash;
     int i;
 
-    password_t chainPassword;
-    strcpy(chainPassword, initialPassword, MAX_PASSWD);
-    chainPassword[MAX_PASSWD] = '\0';
+    strcpy(chainPassword, chain->password);
 
     for(i = 0; i < LENGTH_OF_CHAIN; ++i)
     {
-        checksum(md5_digest, chainPassword);
-        reduce(chainPassword, md5_digest);
+        hash(passwordHash, chainPassword);
+        reduce(chainPassword, passwordHash);
     }
 
-    storeTableRow(initialPassword, md5_digest, LENGTH_OF_CHAIN);
+    memcpy(chain->hash, passwordHash, sizeof(passwordHash));
+    chain->length = i;
 }
 
-int isPasswordUnique(const password_t password)
+// generates initial password for a block of rainbow chains
+static void prepareBlock(struct rainbow_chain *chains)
 {
     int i;
-    for(i = 0; i < NUMBER_OF_PASSWORDS; ++i)
-    {
-        if(uniquePasswords[i] == NULL)
-            return 1;
 
-        if(!strcmp(uniquePasswords[i], password))
-            return 1;
+    for (i = 0; i < CHAINS_IN_BLOCK; ++i)
+    {
+        randomString(chains[i].password);
     }
+}
+
+// generates all rainbow chains in a block of rainbow chains
+static void processBlock(struct rainbow_chain *chains)
+{
+    int i;
+
+#if OPENMP_MODE
+    #pragma omp parallel for
+    for(i = 0; i < CHAINS_IN_BLOCK; ++i)
+#elif CILK_MODE
+    cilk_for (i = 0; i < CHAINS_IN_BLOCK; ++i)
+#else
+    for(i = 0; i < CHAINS_IN_BLOCK; ++i)
+#endif
+    {
+        generateRainbowTableChain(&chains[i]);
+    }
+}
+
+// saves a block of random chains to file
+static void saveBlock(struct rainbow_chain *chains)
+{
+    int i;
+
+    for (i = 0; i < CHAINS_IN_BLOCK; ++i)
+    {
+        storeTableChain(&chains[i]);
+    }
+}
+
+// entry point of the program
+int main(int argc, char **argv)
+{
+    uint64_t startTime, totalTime;
+    struct rainbow_chain *chains;
+    float workTimeSeconds;
+    int i;
+
+#if OPENMP_MODE
+    printf("OpenMP mode selected.\n");
+#elif CILK_MODE
+    printf("Intel Cilk+ mode selected.\n");
+#else
+    printf("No parallel mode selected.\n");
+#endif
+
+    FILE* file = fopen(TABLE_FILE, "wb");
+    if(!file)
+    {
+        perror("Error opening file");
+        return -1;
+    }
+    fclose(file);
+
+    // init program
+    startTime = measureTime(0);
+
+    chains = malloc(CHAINS_IN_BLOCK * sizeof(*chains));
+    assert(chains);
+
+    srand(time(NULL));
+
+    for (i = 0; i < NUMBER_OF_BLOCKS; ++i) {
+        printf("Processing block %d of %d...\n", i, NUMBER_OF_BLOCKS);
+        prepareBlock(chains);
+        processBlock(chains);
+        saveBlock(chains);
+    }
+
+    totalTime = measureTime(startTime);
+    workTimeSeconds = (float)totalTime / USEC_PER_SEC;
+    printf("Work time: %.2f sec\n", workTimeSeconds);
+
+    free(chains);
 
     return 0;
-}
-
-void saveUniquePassword(const password_t password)
-{
-    int i;
-    for(i = 0; i < NUMBER_OF_PASSWORDS; ++i)
-    {
-        if(uniquePasswords[i])
-            continue;
-
-        uniquePasswords[i] = (const char*) malloc(MAX_PASSWD + 1);
-        strcpy(uniquePasswords[i], password);
-        break;
-    }
 }
