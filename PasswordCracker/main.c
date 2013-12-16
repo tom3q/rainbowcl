6160dc048dc5eea8b9a53b2b359d0040f49a263a
Tomasz Figa <tomasz.figa@gmail.com>
2013-12-16 02:06:29 +0100

    Add implementation of password cracker tool

--- PasswordCracker/main.c
+++ PasswordCracker/main.c
@@ -1,8 +1,134 @@
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(void)
+#include "rainbow_chain.h"
+#include "utils.h"
+
+static FILE *f;
+
+static int chainCompare(const void *a, const void *b)
 {
-    printf("Hello world");
+    const struct rainbow_chain *key = a;
+    unsigned long index = (unsigned long)b - 1;
+    struct rainbow_chain chain;
+    size_t ret;
 
+    fseek(f, index * sizeof(chain), SEEK_SET);
+    ret = fread(&chain, sizeof(chain), 1, f);
+    assert(ret == 1);
+
+    return memcmp(key->hash, chain.hash, sizeof(key->hash));
+}
+
+static void stringToHash(hash_t out, const char *in)
+{
+    char strByte[3];
+    uint32_t word;
+    int i;
+
+    strByte[2] = '\0';
+
+    for (i = 0; i < 4; ++i) {
+        const char *strWord = &in[i * 8];
+        uint32_t byte;
+        int j;
+
+        word = 0;
+
+        for (j = 0; j < 4; ++j) {
+            memcpy(strByte, strWord + 2 * j, 2);
+            sscanf(strByte, "%02x", &byte);
+            word >>= 8;
+            word |= byte << 24;
+        }
+
+        out[i] = word;
+    }
+}
+
+static int lookupChain(FILE *f, const hash_t initialHash,
+                       unsigned long index, password_t password,
+                       uint32_t passwordLength, uint32_t chainLength)
+{
+    struct rainbow_chain chain;
+    size_t read;
+    uint32_t i;
+
+    fseek(f, index * sizeof(chain), SEEK_SET);
+    read = fread(&chain, sizeof(chain), 1, f);
+    assert(read == 1);
+
+    hash(chain.hash, chain.password);
+    for (i = 0; i < chainLength; ++i) {
+        if (!memcmp(chain.hash, initialHash, sizeof(chain.hash)))
+            goto found;
+
+        reduce(chain.password, chain.hash, passwordLength, i);
+        hash(chain.hash, chain.password);
+    }
+
+    if (!memcmp(chain.hash, initialHash, sizeof(chain.hash)))
+            goto found;
+
+    return 0;
+
+found:
+    strcpy(password, chain.password);
+    return 1;
+}
+
+int main(int argc, char **argv)
+{
+    struct rainbow_chain chain;
+    uint32_t passwordLength;
+    uint32_t chainLength;
+    unsigned long index;
+    hash_t initialHash;
+    size_t read;
+    uint32_t i;
+    long len;
+
+    f = fopen(argv[1], "rb");
+    assert(f);
+
+    read = fread(&chain, sizeof(chain), 1, f);
+    assert(read == 1);
+
+    passwordLength = strlen(chain.password);
+    stringToHash(initialHash, argv[2]);
+    chainLength = atoi(argv[3]);
+
+    fseek(f, 0, SEEK_END);
+    len = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    len /= sizeof(chain);
+
+    printf("Found %ld rainbow chains in table\n", len);
+
+    for (i = 0; i <= chainLength; ++i) {
+        uint32_t j;
+
+        memcpy(chain.hash, initialHash, sizeof(initialHash));
+
+        for (j = 0; j < i; ++j) {
+            reduce(chain.password, chain.hash,
+                    passwordLength, chainLength - i + j);
+            hash(chain.hash, chain.password);
+        }
+
+        index = (unsigned long)bsearch(&chain, (void *)1, len, 1, chainCompare);
+        if (index && lookupChain(f, initialHash, index - 1,
+                                chain.password, passwordLength, chainLength))
+            break;
+    }
+
+    if (i > chainLength)
+        printf("Failed to find password for given hash\n");
+    else
+        printf("Found password: %s\n", chain.password);
+
+    fclose(f);
     return 0;
 }
