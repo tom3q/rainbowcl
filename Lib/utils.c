a72f919ece2e27036e710bc9bf4b8b3c672ee4b8
Tomasz Figa <tomasz.figa@gmail.com>
2013-12-17 13:58:12 +0100

    Make the reduction function use prebuilt reduction table

--- Lib/utils.c
+++ Lib/utils.c
@@ -1,8 +1,27 @@
 #include "utils.h"
 
+// user-defined constant
 const char charset[CHARSET_SIZE] = "0123456789,."
                                    "abcdefghijklmnopqrstuvwxyz"
                                    "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// generated from charset above using ReduceGenerator
+const char reductionMap[REDUCTION_TABLE_SIZE] =
+                                    "lWLJHIQP0R0nZE0B2y5RYdU6OdSvfkIs"
+                                    "1Xf0AZLmpkxKoHsc97DcZG6vgh4nb5uK"
+                                    "RlQquB8GbOwBEpBggTvzUS6OTl9cAw3."
+                                    "oZhyAg3M1a25V84xGQ1zpf5l4gMGtWSE"
+                                    "N,ZiM6cPnp6,svNxLWCq6Qcy32kYWCQm"
+                                    "ElvV38NNB8ehWm,2rPyLTrMPqHVxakrb"
+                                    "IUNoZpGdru1xVwz.,f19tOHizRATqrzX"
+                                    "afhFS1ChYLsK7rqmOF2IJDXjCXj1Sim3"
+                                    "ur7TUoMPs87VegTPEpA5BPwdDgtAVHwF"
+                                    "SxCKOSzL.Rnv,xikuZIz2jtbbN3lwUoE"
+                                    "kh9FV4EIoHvqaJ80bUejt5DDjmo9aKc0"
+                                    "Hh,MsTGnFYyS4RemCLDFUn9WENBJqOYc"
+                                    "Kafliv7OJeQe5rF7g76Y12MHdAaDuGft"
+                                    "8yKp8XV7yPJsL,jI..b2liyk.RXqXWQu"
+                                    "zhpj.u9WfNwBoaM53YA0iQnsebYXT,Cc"
+                                    "6t.GdjxFdmC4i93RK0Z4DwkJU4dneItJ";
 
 void reduce(password_t out, hash_t const in, size_t length, uint32_t salt)
 {
@@ -13,10 +32,10 @@ void reduce(password_t out, hash_t const in, size_t length, uint32_t salt)
         unsigned int j;
 
         for (j = 0; j < 3 && 3 * i + j < length; ++j) {
-            uint32_t index = word % (8 * CHARSET_SIZE);
+            uint32_t index = word % REDUCTION_TABLE_SIZE;
 
-            out[3 * i + j] = charset[index % CHARSET_SIZE];
-            word /= (8 * CHARSET_SIZE);
+            out[3 * i + j] = reductionMap[index];
+            word /= REDUCTION_TABLE_SIZE;
         }
     }
 }
